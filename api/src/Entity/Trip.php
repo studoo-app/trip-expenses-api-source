73fd0c4c48acbedba57328c77679ccbf3d214a42
JPechberty <j.pechberty@gmail.com>
2025-02-16 00:16:28 +0100

    add normalizationContext to Trip Entity

--- api/src/Entity/Trip.php
+++ api/src/Entity/Trip.php
@@ -6,29 +6,35 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TripRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TripRepository::class)]
 #[ApiResource(
-    #security: "is_granted('ROLE_USER')"
+    #security: "is_granted('ROLE_USER')",
+    normalizationContext: ['groups' => ['trip:read']],
 )]
 class Trip
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['trip:read'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     #[Assert\NotBlank(message: 'Please enter a departure date')]
+    #[Groups(['trip:read'])]
     private ?\DateTimeInterface $departureDate = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank(message: 'Please enter destination')]
+    #[Groups(['trip:read'])]
     private ?string $destination = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
+    //#[Groups(['trip:read'])]
     private ?User $user = null;
 
     public function getId(): ?int
