740fc62ab833cfe79e1b3649f49f992ba5296d23
JPechberty <j.pechberty@gmail.com>
2025-02-15 22:32:22 +0100

    init api

---
+++ api/src/Entity/Trip.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TripRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TripRepository::class)]
+#[ApiResource]
+class Trip
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $departureDate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $destination = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDepartureDate(): ?\DateTimeInterface
+    {
+        return $this->departureDate;
+    }
+
+    public function setDepartureDate(\DateTimeInterface $departureDate): static
+    {
+        $this->departureDate = $departureDate;
+
+        return $this;
+    }
+
+    public function getDestination(): ?string
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(string $destination): static
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+}
